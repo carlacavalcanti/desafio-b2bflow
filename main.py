040cdc2eebffb4d19ad0eaa65dde9b907c8c62e6
Carla Cavalcanti <carlinhacavalcanti17@gmail.com>
2026-06-17 09:42:25 -0300

    Trata erro no fluxo

--- main.py
+++ main.py
@@ -18,15 +18,22 @@ resposta= supabase.table("contatos").select("*").limit(3).execute()
 contatos = resposta.data
 
 for contato in contatos:
-    mensagem = f"Olá, {contato['nome']} tudo bem com você?"
+    try: 
+        mensagem = f"Olá, {contato['nome']} tudo bem com você?"
 
-    url = f"https://api.z-api.io/instances/{instance_id}/token/{token}/send-text"
+        url = f"https://api.z-api.io/instances/{instance_id}/token/{token}/send-text"
 
-    dados_mensagem = {
+        dados_mensagem = {
         "phone": contato["telefone"],
         "message": mensagem
-    }
+         }
 
-    envio = requests.post(url,json=dados_mensagem)
-    print( f"Mensagem enviada para {contato['nome']} - Status: {envio.status_code}")
-  
+        envio = requests.post(url,json=dados_mensagem)
+
+        if envio.status_code != 200:
+            print(f"Erro ao enviar para {contato['nome']}: {envio.text}")
+        else: 
+            print( f"Mensagem enviada para {contato['nome']} - Status: {envio.status_code}")
+    
+    except Exception as e: 
+        print(f"Erro inesperado para {contato['nome']}: {e}")
